948ea2c2dfb17d0fd917ac410424b7c3c7225733
NayZ.Sodium <null>
2021-09-06 21:41:21 +0200

    Fix docker

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-echo "======================================= | Récupération de l'image nayzbysodium/games-tracker sur le DOCKER HUB | ======================================="
+echo "===================== | Récupération de l'image nayzbysodium/games-tracker sur le DOCKER HUB | ====================="
 docker pull nayzbysodium/games-tracker
 echo "======================================= | Arrêt des services existent | ======================================="
 docker-compose down
