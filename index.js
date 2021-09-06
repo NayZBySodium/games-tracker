98ba237e26ac8c680fffce165421b002b145856b
NayZ.Sodium <null>
2021-09-06 22:21:29 +0200

    Fix

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ setInterval(() => {
     console.log(`[INFO]: Updating Rocket League statistics, next update at ${getNextUpdate()}...`);
     require(`./trackers/rocketleague.tracker`);
     console.log(`[INFO]: Updating Rocket League statistics finished!`);
-}, 1000 * 60 * process.env.UPDATE_INTERVAL);
+}, 1000 * 60 * 5);
 
 setInterval(() => {
     console.log(`[INFO]: Updating Warzone statistics, next update at ${getNextUpdate()}`);
     require(`./trackers/warzone.tracker`);
     console.log(`[INFO]: Updating Warzone League warzone finished!`);
-}, 1000 * 60 * process.env.UPDATE_INTERVAL);
+}, 1000 * 60 * 5);
 
-console.log(`[INFO]: App started at ${getHours()}. The stats while be updating all ${process.env.UPDATE_INTERVAL} minutes. (test docker)`);
+console.log(`[INFO]: App started at ${getHours()}. The stats while be updating all ${5} minutes. (test docker)`);
 
 function getNextUpdate() {
     var d = new Date();
@@ -21,7 +21,7 @@ function getNextUpdate() {
     var hours = d.getHours();
     var sec = d.getSeconds();
 
-    min = min + process.env.UPDATE_INTERVAL;
+    min = min + 5;
 
     if (min >= 60) {
         hours = hours + 1;
