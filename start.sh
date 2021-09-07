1a42388e0cec57f4015b850716cadd6f0a1fa946
NayZ.Sodium <55506519+NayZBySodium@users.noreply.github.com>
2021-09-07 19:06:25 +0200

    Amélioration start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "===================== | Récupération de l'image nayzbysodium/games-tracker sur le DOCKER HUB | ====================="
-docker pull nayzbysodium/games-tracker
+docker pull nayzbysodium/games-tracker:latest
 echo "======================================= | Arrêt des services existent | ======================================="
 docker-compose down
 echo "======================================= | Démarrage des services | ======================================="
